db15514dd84155ddd0120c085d109f5cb9c8bff9
Dallair220 <93786532+Dallair220@users.noreply.github.com>
2023-03-26 17:58:55 +0200

    Add winner congratulation

--- script.js
+++ script.js
@@ -1,46 +1,76 @@
-// store players in objects
-// create an object to control the flow of the game
+/* eslint-disable no-alert */
 
 // MAIN GOAL: as little global code as possible
 // -> use modules and factory.
 
 const gameBoardModule = (() => {
-  const board = {
+  let board = {
     row1: ['', '', ''],
     row2: ['', '', ''],
     row3: ['', '', ''],
   };
-
   const boardToArray = () => board.row1.concat(board.row2).concat(board.row3);
 
-  const isPlayerOneNext = true;
+  let isPlayerOneNext = true;
 
+  const restart = () => {
+    board = {
+      row1: ['', '', ''],
+      row2: ['', '', ''],
+      row3: ['', '', ''],
+    };
+
+    isPlayerOneNext = true;
+    gameBoardModule.over = false;
+  };
+
+  let over = false;
   const isOver = () => {
-    let over = false;
+    let winMarker = '';
 
     // check vertical win
     Object.keys(board).forEach((key) => {
       if (board[key][0] !== '')
-        if (board[key][0] === board[key][1] && board[key][0] === board[key][2])
+        if (
+          board[key][0] === board[key][1] &&
+          board[key][0] === board[key][2]
+        ) {
           over = true;
+          // eslint-disable-next-line prefer-destructuring
+          winMarker = board[key][0];
+        }
     });
 
     // check horizontal win
     for (let i = 0; i < 3; i += 1) {
       if (board.row1[i] !== '')
-        if (board.row1[i] === board.row2[i] && board.row1[i] === board.row3[i])
+        if (
+          board.row1[i] === board.row2[i] &&
+          board.row1[i] === board.row3[i]
+        ) {
           over = true;
+          winMarker = board.row1[i];
+        }
     }
 
     // check diagonal win #1
     if (board.row1[0] !== '')
-      if (board.row1[0] === board.row2[1] && board.row1[0] === board.row3[2])
+      if (board.row1[0] === board.row2[1] && board.row1[0] === board.row3[2]) {
         over = true;
+        // eslint-disable-next-line prefer-destructuring
+        winMarker = board.row1[0];
+      }
 
     // check diagonal win #2
     if (board.row3[0] !== '')
-      if (board.row3[0] === board.row2[1] && board.row3[0] === board.row1[2])
+      if (board.row3[0] === board.row2[1] && board.row3[0] === board.row1[2]) {
         over = true;
+        // eslint-disable-next-line prefer-destructuring
+        winMarker = board.row3[0];
+      }
+
+    // eslint-disable-next-line no-use-before-define
+    if (over) displayControllerModule.congratulateWinner(winMarker);
 
     return over;
   };
@@ -50,6 +80,7 @@ const gameBoardModule = (() => {
     board,
     isPlayerOneNext,
     isOver,
+    restart,
   };
 })();
 
@@ -91,7 +122,6 @@ const playerFF = (name, isPlayerOne) => {
           gameBoardModule.isPlayerOneNext = !gameBoardModule.isPlayerOneNext;
         }
       }
-
       // eslint-disable-next-line no-use-before-define
       displayControllerModule.draw();
     });
@@ -106,6 +136,8 @@ const playerFF = (name, isPlayerOne) => {
 };
 
 const displayControllerModule = (() => {
+  // const p1Name = prompt('Player 1 name: ');
+  // const p2Name = prompt('Player 2 name: ');
   const p1 = playerFF('Player 1', true);
   const p2 = playerFF('Player 2', false);
   const fields = document.querySelectorAll('.container > div');
@@ -123,5 +155,22 @@ const displayControllerModule = (() => {
     });
   };
 
-  return { draw };
+  document.querySelector('.restart').addEventListener('click', () => {
+    gameBoardModule.restart();
+    draw();
+  });
+
+  const congratulateWinner = (marker) => {
+    const winner = marker === 'X' ? p1 : p2;
+    alert(`${winner.name} did win!`);
+  };
+
+  return {
+    draw,
+    congratulateWinner,
+  };
 })();
+
+// TODO:
+// 1. fix restart button
+// 2. add UI for winner
